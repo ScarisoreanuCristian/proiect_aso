8c1759afe034f03ebd8c6c70e26ba1ae65ea8884
Scarisoreanu <cristianscarisoreanu59@gmail.com>
2023-01-08 19:53:35 +0200

    Small refactoring

--- scrumboard/routing.py
+++ scrumboard/routing.py
@@ -1,6 +1,6 @@
 from django.urls import re_path
-from . import consumers
+from scrumboard import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/socket-server/', consumers.ChatConsumer.as_asgi())
+    re_path(r'ws/socket-server/', consumers.ChatConsumer.as_asgi()),
 ]
